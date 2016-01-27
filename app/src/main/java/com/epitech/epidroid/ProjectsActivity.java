8d58446857580edf17aa41544fa8ae55289ce8ef
Faraday <thecatisaspy@gmail.com>
2016-01-27 11:09:57 +0100

    ready to download files.

--- app/src/main/java/com/epitech/epidroid/ProjectsActivity.java
+++ app/src/main/java/com/epitech/epidroid/ProjectsActivity.java
@@ -2,6 +2,7 @@ package com.epitech.epidroid;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -30,11 +31,20 @@ import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Future;
 
 public class ProjectsActivity extends AbstractActivity {
 
@@ -43,9 +53,13 @@ public class ProjectsActivity extends AbstractActivity {
     private CharSequence                mTitle;
     private ArrayList<String>           projList = new ArrayList<String>();
     private ArrayAdapter<String>        projAdapter;
+    private ArrayList<String>           filesList;
+    private ArrayList<String>                filesPath;
+    private ArrayAdapter<String>        filesAdapter;
     private EpiContext                  app;
     private JsonArray                   cleanProjects = new JsonArray();
     private JsonArray                   registeredProjects = new JsonArray();
+    private File currentFile;
 
     /* Spinner */
     private ArrayList<String>           filterList = new ArrayList<String>();
@@ -69,7 +83,7 @@ public class ProjectsActivity extends AbstractActivity {
         mNavigationDrawerFragment.setUp(R.id.navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout));
         projAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, projList);
         filterAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, filterList);
-
+        filesPath = new ArrayList<String>();
 
         ListView projDisp = (ListView)findViewById(R.id.project_title);
         projDisp.setAdapter(projAdapter);
@@ -79,8 +93,7 @@ public class ProjectsActivity extends AbstractActivity {
                 final JsonObject selectedItem;
                 if (selected == 0) {
                     selectedItem = cleanProjects.get(position).getAsJsonObject();
-                }
-                else {
+                } else {
                     selectedItem = registeredProjects.get(position).getAsJsonObject();
                 }
                 openPopup(selectedItem);
@@ -101,8 +114,7 @@ public class ProjectsActivity extends AbstractActivity {
                 if (position == 0) {
                     selected = 0;
                     displayAll();
-                }
-                else {
+                } else {
                     selected = 1;
                     displayRegistered();
                 }
@@ -124,6 +136,9 @@ public class ProjectsActivity extends AbstractActivity {
                         displayAll();
                     }
                 });
+        filesList = new ArrayList<String>();
+        filesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, filesList);
+
     }
 
 
@@ -185,7 +200,59 @@ public class ProjectsActivity extends AbstractActivity {
             TextView mod = (TextView)popupView.findViewById(R.id.project_module);
             Button regButton = (Button) popupView.findViewById(R.id.register);
             Button btnDismiss = (Button) popupView.findViewById(R.id.dismiss);
+            ListView files = (ListView)popupView.findViewById(R.id.filesList);
+            files.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                    File file = new File(getFilesDir(), filesList.get(position - 1));
+                    currentFile = file;
+                    try {
+                        file.createNewFile();
+                    }
+                    catch(Exception E) {
+                        E.printStackTrace();
+                    }
+                    file.setReadable(true, false);
+                    System.out.println("https://intra.epitech.eu" + filesPath.get(position - 1));
+                    Ion.with(getApplicationContext())
+                            .load("http://intra.epitech.eu" + filesPath.get(position - 1))
+                            .write(currentFile)
+                            .setCallback(new FutureCallback<File>() {
+                                @Override
+                                public void onCompleted(Exception e, File result) {
+
+                                    try
+                                    {
+                                        BufferedReader in = new BufferedReader(new FileReader(result.getAbsolutePath()));
+                                        String line;
+                                        while((line = in.readLine()) != null)
+                                        {
+                                            System.out.println(line);
+                                        }
+                                        in.close();
+                                    }
+                                    catch (Exception E)
+                                    {
+                                        E.printStackTrace();
+                                    }
+                                    Intent target = new Intent(Intent.ACTION_VIEW);
+                                    target.setDataAndType(Uri.fromFile(result), "application/pdf");
+                                    target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+
+                                    Intent intent = Intent.createChooser(target, "Open File");
+                                    startActivity(intent);
+                                }
+                            });
+                }
+            });
+
+            TextView FileTitle = new TextView(popupView.getContext());
+            files.setAdapter(filesAdapter);
+            files.addHeaderView(FileTitle);
+
+            //filesList.add("encule.pdf");
+            //filesAdapter.notifyDataSetChanged();
 
             btnDismiss.setOnClickListener(new Button.OnClickListener() {
                 @Override
@@ -215,7 +282,31 @@ public class ProjectsActivity extends AbstractActivity {
                 reg.setText("You are not registered");
                 regButton.setText("Register");
             }
-
+            filesList.clear();
+            filesPath.clear();
+            filesAdapter.notifyDataSetChanged();
+            Ion.with(getApplicationContext())
+                    .load(getString(R.string.request_method_get), getString(R.string.api_domain) + getString(R.string.domain_files))
+                    .setBodyParameter(getString(R.string.token), app.token)
+                    .setBodyParameter(getString(R.string.scolarYear), selected.get(getString(R.string.scolarYear)).getAsString())
+                    .setBodyParameter(getString(R.string.codeModule), selected.get(getString(R.string.codeModule)).getAsString())
+                    .setBodyParameter(getString(R.string.codeInstance), selected.get(getString(R.string.codeInstance)).getAsString())
+                    .setBodyParameter(getString(R.string.codeActivity), selected.get(getString(R.string.codeActivity)).getAsString())
+                    .asJsonArray()
+                    .setCallback(new FutureCallback<JsonArray>() {
+                        @Override
+                        public void onCompleted(Exception e, JsonArray result) {
+                            if (result == null) {
+                                return;
+                            }
+                            for (int i = 0; i < result.size(); ++i) {
+                            filesList.add(result.get(i).getAsJsonObject().get("title").getAsString());
+                            filesPath.add(result.get(i).getAsJsonObject().get("fullpath").getAsString());
+                            System.out.println(result.get(i).getAsJsonObject().get("fullpath").getAsString());
+                            }
+                            filesAdapter.notifyDataSetChanged();
+                        }
+                    });
             popupWindow.showAtLocation(popupView, Gravity.RIGHT, Gravity.CENTER, Gravity.CENTER);
 
         } catch (Exception e) {
@@ -237,6 +328,7 @@ public class ProjectsActivity extends AbstractActivity {
             .setCallback(new FutureCallback<JsonObject>() {
                 @Override
                 public void onCompleted(Exception e, JsonObject result) {
+
                 }
             });
             Toast.makeText(getApplicationContext(), getString(R.string.you_registered), Toast.LENGTH_SHORT).show();
@@ -260,6 +352,7 @@ public class ProjectsActivity extends AbstractActivity {
             .setCallback(new FutureCallback<JsonObject>() {
                 @Override
                 public void onCompleted(Exception e, JsonObject result) {
+
                 }
             });
             Toast.makeText(getApplicationContext(), getString(R.string.you_unregistered), Toast.LENGTH_SHORT).show();
